5812c0baa8052cb2e198346464f7a0074aca7772
Cody Lewis <Cody.Lewis@newcastle.edu.au>
2023-11-15 09:42:42 +1100

    Made nvidia gpu detection spot all caps

--- configure.sh
+++ configure.sh
@@ -3,6 +3,6 @@
 poetry install
 
 gpu=$(lspci | grep -i '.* vga .* nvidia .*')
-if [[ $gpu == *' nvidia '* ]]; then
+if [[ $gpu == *' NVIDIA '* ]]; then
     poetry run pip install -U "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 fi
